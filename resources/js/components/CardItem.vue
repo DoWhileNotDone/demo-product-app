8f9102b7cf2b93d4bedacf0bc7354fda2d048a5b
DavegTheMighty <dave@example.com>
2021-07-24 12:52:44 +0100

    Product Demo App - Add Card Component with minimal styling

---
+++ resources/js/components/CardItem.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div class="px-10 py-2">
+    <div class="transition duration-500 ease-in-out rounded bg-white hover:bg-gray-100 transform hover:-translate-y-1 hover:scale-105 w-full hover:no-underline lg:max-w-full lg:flex shadow" >
+      <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover text-center overflow-hidden" :style="`background-image: url('${item.image_url}')`" :title="`Image of ${item.name}`"></div>
+      <div class="p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-2">{{ item.name }}</div>
+          <div class="text-sm">
+            <p class="pt-1 text-gray-900 leading-none">{{ item.brand }}</p>
+            <p class="pt-1 text-gray-900 leading-none">{{ (item.price).toLocaleString('en-GB', {style: 'currency', currency: 'GBP' }) }}</p>
+            <p class="pt-1 text-gray-600">{{ new Date(item.date_added).toLocaleString('en-GB') }}</p>
+          </div>              
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  
+  export default {
+    name: "CardItem",
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    }, 
+  }
+
+</script>
--- resources/js/components/DataList.vue
+++ resources/js/components/DataList.vue
@@ -4,25 +4,17 @@
     <!-- Search Panel -->
     <div>
       <div class="bg-white flex items-center rounded-3x shadow-xl">
-        <input v-model="search" class="rounded-l-full w-full py-4 px-6 text-gray-500 leading-tight focus:outline-none" id="search" type="text" placeholder="Search">
-        <button @click="searchItems" class="bg-green-500 hover:bg-green-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <input v-model="search" class="rounded-l-full w-full py-4 px-6 text-gray-500 leading-tight focus:outline-none" id="search" type="text" placeholder="Search name and brand">
+        <button @click="searchItems" class="bg-green-500 hover:bg-green-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:shadow-outline" type="button">
           Search
         </button>
-        <button @click="clearSearch" class="bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <button @click="clearSearch" class="bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:shadow-outline" type="button">
           Clear
         </button>
       </div>
     </div>
     <!-- List Items -->
-    <div v-for="item in laravelData.data" :key="item.id">
-      <div class="flex flex-col">
-        <div class="bg-white hover:bg-gray-100 border border-white hover:border-gray-100 shadow-lg rounded-3xl p-4 m-4">
-          <div class="flex-none sm:flex text-gray-500 leading-tight">
-            {{ item.name }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <card-item v-for="item in laravelData.data" :key="item.id" :item="item" />
     <pagination :data="laravelData" @pagination-change-page="getData"></pagination>
   </div>
 
@@ -31,11 +23,13 @@
 <script>
   
   import axios from 'axios';
+  import CardItem from './CardItem.vue'
   import Pagination from 'laravel-vue-pagination';
 
   export default {
     name: "DataList",
     components: {
+      CardItem,
       Pagination,
     },
     props: {
